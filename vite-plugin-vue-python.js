01fe3d284becbc0e6d28b255a827c61059f2f80b
Jesus Zerpa <jesus26abraham1996@gmail.com>
2024-02-16 17:14:37 -0400

    uupdate

---
+++ vite-plugin-vue-python.js
@@ -0,0 +1,51 @@
+const { basename, join } = require('path')
+const fs = require('fs')
+
+module.exports = (options = {}) => {
+
+  const { targets = [], hook = 'buildStart',interpreter="python",dirProject=null} = options
+  return {
+    name: 'python-fastfront',
+    [hook]: async() => {
+
+      /*
+      const { exec } = require("child_process");
+      exec(`/home/zerpa/anaconda3/envs/metaverse/bin/python -c "import vbuild; vbuild.build()"`, (error, stdout, stderr) => {
+          if (error) {
+              console.log(`error: ${error.message}`);
+              return;
+          }
+          if (stderr) {
+              console.log(`stderr: ${stderr}`);
+              return;
+          }
+          console.log(`stdout: ${stdout}`);
+      });
+      */
+    },
+
+  transform(src, id) {
+
+    const { execSync } = require("child_process");
+    if (id.endsWith(".ff")){
+      let stdout=execSync(`${interpreter} -c "import vbuild; vbuild.build('${id}','${dirProject}')"`).toString()
+      
+      return {
+        code:stdout
+      }
+    }
+    if (id.endsWith(".py")){
+  
+      let stdout=execSync(`${interpreter} -c "import vbuild; vbuild.src_py2js('${id}','${dirProject}')"`).toString()
+
+      return {
+        code:stdout
+      }
+    }
+    else{
+      //console.log("aaaaaaaa",id)
+    }
+  }
+  
+} 
+}
